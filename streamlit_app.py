c1a4fef0478c9a5cba51539bf047af116dcd942a
Cudnonzeal <CloudAnzen@protonmail.com>
2025-06-15 21:40:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import openai
-from get_clean_html import analyze_url  # <- import funkcji
+from get_clean_html import analyze_url compose_message# <- import funkcji
 
 st.title("Analiza strony HTML przez AI")
 
