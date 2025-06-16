3af57b0f9937a3e65c8541cba56a274e6165c9a1
Cudnonzeal <CloudAnzen@protonmail.com>
2025-06-17 01:46:05 +0200

    Update Save_Des_and_Key.py

--- Save_Des_and_Key.py
+++ Save_Des_and_Key.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-def Save_to_db(url: str, des_key: str):
-    file_path = 'Descripion_Keywords.xlsx'  # Path to your .xlsx file
+def Save_to_db(url, des_key):
+    file_path = 'Descripion_Keywords.csv'  # Path to your .xlsx file
     # Load the Excel file
-    df = pd.read_excel(file_path)
+    df = pd.read_csv(file_path)
     # Check if URL already exists
     if url in df['URL'].values:
         print("URL already in db")
@@ -12,5 +12,5 @@ def Save_to_db(url: str, des_key: str):
         # Add the new row
         df.loc[len(df)] = [url, des_key]
         # Save the DataFrame back to Excel (prevent index column from being saved)
-        df.to_excel(file_path, index=False)
+        df.to_csv(file_path, index=False)
         print("URL and Des_Key added")
