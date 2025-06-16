0207242795716bf1aa2d317d4f0040a965a45853
Cudnonzeal <CloudAnzen@protonmail.com>
2025-06-17 01:30:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,5 +17,6 @@ if st.button("Analizuj stronę"):
                 result = analyze_url(url)
                 st.subheader("Wynik analizy AI:")
                 st.write(result)
+                Save_to_db(url, result)
             except Exception as e:
                 st.error(f"Wystąpił błąd: {e}")
