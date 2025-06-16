2e79565e7ec7d8486e2687355486451353ec01e5
Cudnonzeal <CloudAnzen@protonmail.com>
2025-06-17 01:28:04 +0200

    Update Save_Des_and_Key.py

--- Save_Des_and_Key.py
+++ Save_Des_and_Key.py
@@ -1,1 +1,13 @@
+import pandas as pd
 
+
+def Save_to_db(url: str, des_key: str):
+  file_path = 'Description_Keywords.xlsx'  # Path to your .xlsx file
+  df = pd.read_excel(file_path)
+  if url in df['URL'].values:
+    print("URL already in db")
+    return
+  else:
+    df.loc[len(df)] = [url, des_key]
+    df.to_excel(file_path)
+    print("URL and Des_Key added")
